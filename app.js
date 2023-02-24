c09ba1449602e5ecc8479f1469e7277327c260f6
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-02-24 15:43:29 -0500

    arreglo de cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan'
 import indexRouter from './routes/index.js'
+import cors from 'cors'
 import { __dirname } from './utlis.js';
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 
 app.use('/', indexRouter);
